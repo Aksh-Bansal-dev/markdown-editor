10e27aad76cb6fef3301325408267c1f7abf9b4b
Aksh Bansal <akshmark@gmail.com>
2025-05-02 16:00:07 +0530

    format code to fix code styling

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,8 @@ use wasm_bindgen::prelude::*;
 
 mod parser;
 
-
 #[wasm_bindgen]
-pub fn parse(s: &str)->String {
+pub fn parse(s: &str) -> String {
     let res = parser::parse_helper(s.to_string());
     res
 }
-
-
