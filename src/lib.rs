f89b8d1b6102e18266bc1826dc30ef763d5ee46d
Aksh Bansal <akshmark@gmail.com>
2025-05-02 23:02:18 +0530

    add unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,22 @@ mod parser;
 
 #[wasm_bindgen]
 pub fn parse(s: &str) -> String {
-    let res = parser::parse_helper(s.to_string());
-    res
+    parser::parse_helper(s.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_parses_list() {
+        let result = parse("- hello");
+        assert_eq!(result, "<ul><li>hello</li></ul>");
+    }
+
+    #[test]
+    fn it_parses_heading() {
+        let result = parse("## hello");
+        assert_eq!(result, "<h2>hello</h2>");
+    }
 }
